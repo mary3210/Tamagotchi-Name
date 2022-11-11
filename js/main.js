4389fa008b88214f7569e36cb00c58ccc77a53e1
mary3210 <maryrodri3210@gmail.com>
2022-11-11 13:37:58 -0600

    feed button now interacts with hunger p tag

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ startBtn.addEventListener("click", retrieveName)
 let tamagotchi =  {
 petName, 
 age: 0,
-hunger: 0,
+hunger: 3,
 bordedom: 0,
 sleepiness:0,
 }
@@ -19,13 +19,27 @@ let hunger = document.querySelector("#hunger")
 hunger.innerHTML = `Hunger: ${tamagotchi.hunger}`
 let boredom = document.querySelector("#boredom")
 boredom.innerHTML = `Bordeom: ${tamagotchi.bordedom}`
+let sleepiness = document.querySelector("#sleepiness")
+sleepiness.innerHTML = `Tired: ${tamagotchi.sleepiness}`
+
+
+let feed = document.querySelector('#feed')
+let play = document.querySelector('#play')
+let sleep = document.querySelector('#sleep')
 
 function retrieveName(){
   let input = prompt("What is your pet name?")
   tamagotchi.petName = input
   console.log(tamagotchi.petName)
   this.style.display = "none";
-  
-     
-
 }
+
+  function decreaseStat(btn){
+    if (btn === "food" && tamagotchi.hunger < 10 && tamagotchi.hunger >0){
+      //console.log(tamagotchi.hunger)
+      tamagotchi.hunger = tamagotchi.hunger -1
+      hunger.innerHTML = `Hunger: ${tamagotchi.hunger}`
+      
+    }
+    
+  }
