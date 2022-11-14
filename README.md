ce1b73125e9c277bb3f41e9e297b028b61d72b04
mary3210 <maryrodri3210@gmail.com>
2022-11-13 20:24:28 -0600

    Update README and organize code

--- README.md
+++ README.md
@@ -1,3 +1,31 @@
-# Tamagotchi-Name
+Tamagotchi Game
+----------------
+This project is a game that begins with a plant-like creature on the user's screen. The user starts the game after naming the creature/pet. When the game starts, every 5 seconds the stats; hungry, boredness, and tired increase by 1. The user must decrease the stats by pressing any of the buttons below the stats. If the user does not make it in time to lower the state before hitting "10" on any of the three, the game alerts the user that they have lost. In addition, the pet morphs 2 times after its inital appearance at age 5 and 10.
+
+-------------------------------------------
+
+technologies used:
+html 
+css
+javascript
+node
+git
+--------------------------------------------
+
+Biggest challenges:
+Some of my code is repetitive and I could have used a for loop in the function addToStat()
+I initally tried a for loop but had a hard time figuring out how to permanently update the innerhtml as I was only able to change the value in variables tamagotchi.hungry, tamagotchi.boredness and tamagotchi.sleepiness. 
+
+------------------------------------------------------------------------------
+
+Biggest Triumphs:
+I initially had issues with getting my start button to return a value to me, I found out that addeventlistener can't retrieve a value that easily and in order to be able to I had to access the object to get the data outside the button.
+
+
+-------------------------------------------------------------------------------
+Words of Wisdom: 
+always test your code with every new step
+
+
 
 
--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
 const startBtn = document.querySelector(".startBtn")
 let petName;
-let gameOn = true;
 startBtn.addEventListener("click", retrieveName)
-
 
 let tamagotchi =  { 
 petName,
@@ -11,8 +9,6 @@ hunger: 0,
 boredom: 0,
 sleepiness:0,
 }
-
-
 
 let age = document.querySelector("#age")
 age.innerHTML = `Age: ${tamagotchi.age}`
@@ -23,11 +19,9 @@ boredom.innerHTML = `Boredom: ${tamagotchi.boredom}`
 let sleepiness = document.querySelector("#sleepiness")
 sleepiness.innerHTML = `Tired: ${tamagotchi.sleepiness}`
 
-
 let feed = document.querySelector('#feed')
 let play = document.querySelector('#play')
 let sleep = document.querySelector('#sleep')
-
 let body = document.querySelector('body')
 
 function retrieveName(){
@@ -37,52 +31,38 @@ function retrieveName(){
   this.style.display = "none";
   let nameOfPet = document.querySelector('#nameOfPet')
   nameOfPet.innerHTML = tamagotchi.petName
+  time ()
 }
 
-  function decreaseStat(btn){
-    if (btn === "food" && tamagotchi.hunger < 10 && tamagotchi.hunger >0){
-      //console.log(tamagotchi.hunger)
-      tamagotchi.hunger = tamagotchi.hunger -1
-      hunger.innerHTML = `Hunger: ${tamagotchi.hunger}`
-      
-    }
-    else if(btn ==="play" && tamagotchi.boredom <10 && tamagotchi.boredom >0){
-      tamagotchi.boredom = tamagotchi.boredom -1
-      boredom.innerHTML = `Boredom: ${tamagotchi.boredom}`
-    }
-    else if(btn ==="sleep" && tamagotchi.sleepiness <10 && tamagotchi.sleepiness >0){
-      tamagotchi.sleepiness = tamagotchi.sleepiness -1
-      sleepiness.innerHTML = `Tired: ${tamagotchi.sleepiness}`
-      setTimeout(setBlack, 50)
-      setTimeout(setWhite, 1000)
-      
-    }
-  
+function decreaseStat(btn){
+  if (btn === "food" && tamagotchi.hunger < 10 && tamagotchi.hunger >0){
+    tamagotchi.hunger = tamagotchi.hunger -1
+    hunger.innerHTML = `Hunger: ${tamagotchi.hunger}`
+  } 
+  else if(btn ==="play" && tamagotchi.boredom <10 && tamagotchi.boredom >0){
+    tamagotchi.boredom = tamagotchi.boredom -1
+    boredom.innerHTML = `Boredom: ${tamagotchi.boredom}`
   }
+  else if(btn ==="sleep" && tamagotchi.sleepiness <10 && tamagotchi.sleepiness >0){
+    tamagotchi.sleepiness = tamagotchi.sleepiness -1
+    sleepiness.innerHTML = `Tired: ${tamagotchi.sleepiness}`
+    setTimeout(setBlack, 50)
+    setTimeout(setWhite, 1000)
+    }
+}
 
-  function setBlack(){
-    body.style.backgroundColor = 'black'
+function setBlack(){
+  body.style.backgroundColor = 'black'
   }
 function setWhite(){
   body.style.backgroundColor ="white"
 }
 
 function time(){
-
 let gameInterval = setInterval(addToStat, 5000)
 
-
 function addToStat(){
-  
-  //let stats = [tamagotchi.hunger, tamagotchi.age, tamagotchi.boredom, tamagotchi.sleepiness]
-  //for( i=0; i < stats.length; i ++){
-  //  console.log("index " + i)
-  //  console.log("stat value " + stats[i])
-  //  stats[i] += 1
-   // console.log("updated stat value " + stats[i])
-  //updateTama()
 
-  //}
   tamagotchi.hunger+= 1
   hunger.innerHTML = `Hunger: ${tamagotchi.hunger}`
 
@@ -98,7 +78,7 @@ function addToStat(){
 changeImg()
   if(tamagotchi.boredom == 10 || tamagotchi.hunger == 10 ||  tamagotchi.sleepiness == 10){
     clearInterval(gameInterval)
-    console.log("You lost!")
+    alert("You lost!")
   }
 }
 }
@@ -119,6 +99,6 @@ function changeImg(){
 
 
 
-time()
+
 
 
